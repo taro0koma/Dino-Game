886b57e3affe703ac823e14b2fe91b16e2bad7fc
YURI <53067551+taro0koma@users.noreply.github.com>
2024-03-29 18:56:14 +0900

    Update dinoGame.js

--- dinoGame.js
+++ dinoGame.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const dinoImage = new Image();
-dinoImage.src = 'image/dino.png';
+dinoImage.src = 'dino.png';
 dinoImage.onload = () => {
     ctx.drawImage(dinoImage,0,320);
 };
